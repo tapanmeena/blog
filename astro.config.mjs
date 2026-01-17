49d1eecab5b3991ada8de0af0f38616792af6b69
tapanmeena <tapanmeena1998@gmail.com>
2026-01-17 15:41:37 +0530

    feat: Add Header and Footer components, restructure layout
    - Implemented Header component with navigation and theme toggle.
    - Created Footer component with branding and navigation links.
    - Removed Navigation component and integrated Header in BaseLayout.
    - Updated BaseLayout to include Header and Footer.
    - Added new blog pages for categories and tags with dynamic routing.
    - Introduced global CSS file for styling.
    - Updated TypeScript configuration for JSX support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tapanmeena.com",
-  base: "/blog",
   output: "static",
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
